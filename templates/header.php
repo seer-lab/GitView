cb54c1742d8347a35396f731233f0dddcae65d57
Joseph <heron.joseph@gmail.com>
2014-05-27 10:43:23 -0400

    REFACTORED: last js library
    Also, added some long over due git ignores. 1 for ignoring the parsing log output folder and the second for ignoring the js/lib folder (used to hold local copies of js libraries for testing purposes).

--- templates/header.php
+++ templates/header.php
@@ -25,7 +25,7 @@
 
     <!-- HTML5 shiv, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
-      <script src="js/html5shiv.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js"></script>
     <![endif]-->
   </head>
   <body>
